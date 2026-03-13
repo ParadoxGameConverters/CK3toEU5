4af7ce3127e1a838bbd80fd0b8b21726ae6fe325
Idhrendur <idhrendur@hotmail.com>
2026-03-12 21:14:07 -0700

    Do a little work

--- src/ck3_to_eu5_converter.hpp
+++ src/ck3_to_eu5_converter.hpp
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef SRC_CK3_TO_EU5_CONVERTER_HPP
+#define SRC_CK3_TO_EU5_CONVERTER_HPP
 
 
 
@@ -11,3 +12,7 @@ namespace ck3_to_eu5
 void ConvertCk3ToEu5();
 
 }  // namespace ck3_to_eu5
+
+
+
+#endif  // SRC_CK3_TO_EU5_CONVERTER_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,27 @@
+#include <external/commonItems/Log.h>
+
+#include <exception>
 #include <print>
 
 #include "ck3_to_eu5_converter.hpp"
 
 
 
-int main()
+int main() // NOLINT(bugprone-exception-escape)
 {
    try
    {
+      Log(LogLevel::Progress) << "0%";
+
       // I miss printf()
       // Meet C++23's new hotness!
-
       std::println("Hello, world!");
       ck3_to_eu5::ConvertCk3ToEu5();
    }
-   catch (...)
+   catch (const std::exception& e)
    {
-      std::println("An error occurred.");
+      Log(LogLevel::Error) << e.what();
+      return -1;
    }
 
    return 0;
