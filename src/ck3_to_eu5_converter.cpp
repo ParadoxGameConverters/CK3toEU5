931fd4050165662186aff64dc1c4a3e273efeebb
Idhrendur <idhrendur@hotmail.com>
2026-03-12 20:27:23 -0700

    Set up an initial build.

---
+++ src/ck3_to_eu5_converter.cpp
@@ -0,0 +1,14 @@
+#include <external/commonItems/Log.h>
+
+
+
+namespace
+{
+
+void ConvertVic3ToHoi4()
+{
+   Log(LogLevel::Progress) << "100%";
+   Log(LogLevel::Notice) << "* Conversion complete *";
+}
+
+}  // namespace
---
+++ src/placeholder_tests.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+
+
+
+TEST(PlaceholderTests, PlaceholderTest)
+{
+   SUCCEED();
+}
